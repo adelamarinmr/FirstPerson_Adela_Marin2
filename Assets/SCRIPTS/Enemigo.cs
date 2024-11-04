4eb187a9770d6b5ff2d7491f2d54bd6e5d79d5e2
adelamarinmr <adelamarinmr@gmail.com>
2024-11-04 16:09:16 +0100

    mucho texto

--- Assets/SCRIPTS/Enemigo.cs
+++ Assets/SCRIPTS/Enemigo.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,10 +8,12 @@ public class Enemigo : MonoBehaviour
 {
     private NavMeshAgent agent;
     private FirstPerson player;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
        agent = GetComponent<NavMeshAgent>();
+       anim =  GetComponent<Animator>();
 
         player = GameObject.FindObjectOfType<FirstPerson>();
     }
@@ -20,5 +23,28 @@ public class Enemigo : MonoBehaviour
     {
         //tengo que definir cpomo destino la posición del player
         agent.SetDestination(player.transform.position);
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+
+            agent.isStopped = true; // me paro ante él
+
+            //Activar animación ataque
+
+            anim.SetBool("Attack", true);
+        }
+
     }
+
+    #region Eventos de animacion
+    private void FinAtaque()
+    {
+        //termino de atacar, vuelvo a moverme
+        agent.isStopped = false;
+        anim.SetBool("Attack", false);
+    }
+    #endregion
+
+
 }
+
