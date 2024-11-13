f1e6751ce975ff3fc0b557048f7147f717be052f
adelamarinmr <adelamarinmr@gmail.com>
2024-11-13 19:42:17 +0100

    ApuntesCorrutinas

---
+++ Assets/SCRIPTS/ApuntesCorrutinas.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApuntesCorrutinas : MonoBehaviour
+{
+     // es un metodo con cortes, que devuelven una palabra que es -> IEnumerator
+    // los cortes van con -> yield return new WaitForSeconds
+   // yield= es como hasta dentro de un rato y`puedes poner los que te den la gana
+    //void Start()
+    //{
+
+    //   StartCoroutine(Semaforo());
+    //}
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    //IEnumerator Semaforo()
+    //{
+    //    Debug.Log("Verde");
+    //    yield return new WaitForSeconds(2);
+    //    Debug.Log("Amarillo");
+    //    yield return new WaitForSeconds(3);
+    //    Debug.Log("Rojo");
+
+    //}
+}
--- Assets/SCRIPTS/SistemaInteracciones.cs
+++ Assets/SCRIPTS/SistemaInteracciones.cs
@@ -6,38 +6,39 @@ public class SistemaInteracciones : MonoBehaviour
 {
     private Camera cam;
     [SerializeField] private float distanciaInteraccion;
-   private Transform interactuableActual;
+    private Transform interactuableActual;
     void Start()
     {
-        cam= Camera.main;
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, distanciaInteraccion))
-       {
-            if(hit.transform.TryGetComponent(out CajaMunición scriptCaja))
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, distanciaInteraccion))
+        {
+            if (hit.transform.TryGetComponent(out CajaMunición scriptCaja))
             {
-                interactuableActual= hit.transform;
-                interactuableActual.GetComponent<Outline>().enabled=true;
-                    
+                interactuableActual = hit.transform;
+                interactuableActual.GetComponent<Outline>().enabled = true;
 
-                if(Input.GetKeyDown(KeyCode.E)) 
+
+                if (Input.GetKeyDown(KeyCode.E))
                 {
                     scriptCaja.Abrir();
                 }
-           
-       }
 
-        //si tenia un interactuable 
-        else if (interactuableActual)
-        {   //le apago...
-            interactuableActual.GetComponent<Outline>().enabled = false;
+            }
 
-            //Le anulo
-            interactuableActual = null;
+            //si tenia un interactuable 
+            else if (interactuableActual)
+            {   //le apago...
+                interactuableActual.GetComponent<Outline>().enabled = false;
+
+                //Le anulo
+                interactuableActual = null;
+            }
+
         }
-
     }
 }
