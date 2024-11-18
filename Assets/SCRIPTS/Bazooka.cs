0aa0481d10b28920f2d9a3329459e7f06009d2a8
adelamarinmr <adelamarinmr@gmail.com>
2024-11-18 17:05:36 +0100

    a

--- Assets/SCRIPTS/Bazooka.cs
+++ Assets/SCRIPTS/Bazooka.cs
@@ -9,7 +9,7 @@ public class Bazooka : MonoBehaviour
     
     void Start()
     {
-        
+       
     }
 
     
---
+++ Assets/SCRIPTS/Grenade.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    [SerializeField] private float fuerzaImpulso;
+    [SerializeField] private float tiempoVida;
+
+
+    [SerializeField] private LayerMask queEsDanhable;
+    [SerializeField] private float radioExplosion;
+
+    [SerializeField] GameObject explosionPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward.normalized*fuerzaImpulso, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+
+
+        //que la granada detecte todos los colliders dañables en un cierto radio de explosion, incluir un if
+        // un if para ver si al menos hemos dañado un collider
+       Collider[] collsDetectados= Physics.OverlapSphere(transform.position, radioExplosion, queEsDanhable);
+
+        if(collsDetectados.Length > 0)
+        {
+            Debug.Log("Adios enemy");
+        }
+    }
+}
