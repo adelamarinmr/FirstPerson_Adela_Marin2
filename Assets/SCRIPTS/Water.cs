09adf179e6465c53a447450aae3bb7ef8f8ffedb
adelamarinmr <adelamarinmr@gmail.com>
2024-11-21 21:22:23 +0100

    a

---
+++ Assets/SCRIPTS/Water.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Water : MonoBehaviour
+{
+
+    private Volume efecto;
+    // Start is called before the first frame update
+    void Start()
+    {
+       efecto= GetComponent<Volume>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (efecto.profile.TryGet(out LensDistortion distorsion))
+        {
+            //distorsion.xMultiplier
+        }
+    }
+}
