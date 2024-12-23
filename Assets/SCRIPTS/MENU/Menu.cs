1bb21735605e74917cf414677131dda626f4c0cd
adelamarinmr <adelamarinmr@gmail.com>
2024-12-23 14:11:02 +0100

    1

--- Assets/SCRIPTS/MENU/Menu.cs
+++ Assets/SCRIPTS/MENU/Menu.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    public void Play()
     {
-        
+        SceneManager.LoadScene("GAME");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
+
+    public void CargarNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+
 }
