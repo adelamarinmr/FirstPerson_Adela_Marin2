fa670388d6f47f5bdd335d7b3f9395360a5419ef
adelamarinmr <adelamarinmr@gmail.com>
2024-12-23 14:40:16 +0100

    YA

---
+++ Assets/SCRIPTS/MENU/Settings.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void CargarNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+
+}
