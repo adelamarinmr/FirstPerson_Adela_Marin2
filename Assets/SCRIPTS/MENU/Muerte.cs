c2666c34bee0fc693d8a48458ccb13c26599918d
adelamarinmr <adelamarinmr@gmail.com>
2025-01-06 17:48:41 +0100

    aa

--- Assets/SCRIPTS/MENU/Muerte.cs
+++ Assets/SCRIPTS/MENU/Muerte.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Muerte : MonoBehaviour
 {
+    public float timeRemaining = 30f; // Tiempo inicial en segundos
+    public Text timerText; // Referencia al componente UI Text
+    private bool isGameOver = false; // Controla si el juego terminó
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +24,40 @@ public class Muerte : MonoBehaviour
         {
             SceneManager.LoadScene(1);
         }
+
+
+
+        if (!isGameOver)
+        {
+            // Actualizar el temporizador
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime; // Reducir el tiempo restante
+                UpdateTimerUI(); // Actualizar el texto en pantalla
+            }
+            else
+            {
+                // Cuando el tiempo se agota
+                timeRemaining = 0;
+                isGameOver = true;
+                GameOver();
+            }
+        }
+    }
+
+
+
+    void UpdateTimerUI()
+    { // Actualizar el texto del temporizador en formato HH:MM:SS
+        int hours = Mathf.FloorToInt(timeRemaining / 3600);
+        int minutes = Mathf.FloorToInt((timeRemaining % 3600) / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+    }
+
+    void GameOver()
+    {
+        // Cambiar a la escena de Game Over
+        SceneManager.LoadScene("Muerte");
     }
 }
